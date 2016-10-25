c63ec8009be3d9110ad2e383f651e44d35910bfa
Jasmine Singh <jasmine.singh@jas.si>
2016-10-25 15:17:08 -0700

    changed /apps/pop to use Class-Based Views

--- baby_names/apps/pop/urls.py
+++ baby_names/apps/pop/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from .views import Index, Filter
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^filter$', views.filter, name="filter"),
+    url(r'^$', Index.as_view(), name="index"),
+    url(r'^filter$', Filter.as_view(), name="filter"),
 ]
--- baby_names/apps/pop/views.py
+++ baby_names/apps/pop/views.py
@@ -1,52 +1,51 @@
 from django.shortcuts import render, redirect, reverse
+from django.views.generic import View
 from ..faves.models import Frequency
 
-# Create your views here.
-def index(request):
-	names = Frequency.objects.filter(state="CA",gender="M")[:10]
-	# newNames = []
-	# for i in range(0,10):
-	# 	newNames.append(names[i])
+class Index(View):
+	def get(self, request):
+		names = Frequency.objects.filter(state="CA",gender="M")[:10]
+		# newNames = []
+		# for i in range(0,10):
+		# 	newNames.append(names[i])
 
-	states = Frequency.objects.raw("select distinct state as id, state from frequency order by state")
-	# print allStates
-	# for s in allStates:
-	# 	print s.state
+		states = Frequency.objects.raw("select distinct state as id, state from frequency order by state")
+		# print allStates
+		# for s in allStates:
+		# 	print s.state
 
-	# states = [
-	# 	{'name': 'AK'},
-	# 	{'name': 'CA'}
-	# 	]
+		# states = [
+		# 	{'name': 'AK'},
+		# 	{'name': 'CA'}
+		# 	]
 
-	context = {
-		'names': names,
-		'states': states
-	}
-	return render(request, 'pop/index.html', context)
+		context = {
+			'names': names,
+			'states': states
+		}
+		return render(request, 'pop/index.html', context)
 
-def filter(request):
-	print "Running filter..."
-	query = "SELECT name as id, name, sum(count) as count FROM frequency"
-	whereclause = " WHERE id>0"
-	groupby = " GROUP BY name ORDER BY sum(count) desc"
-	name_dict = []
+class Filter(View):
+	def get(self, request):
+		print "Running filter..."
+		query = "SELECT name as id, name, sum(count) as count FROM frequency"
+		whereclause = " WHERE id>0"
+		groupby = " GROUP BY name ORDER BY sum(count) desc"
+		name_dict = []
 
-	if 'gender' in request.GET and request.GET['gender']!="":
-		whereclause += " and gender=%s"
-		name_dict.append(request.GET['gender'])
-		# name_dict['gender'] = request.GET['gender']
-	if request.GET['state']!="":
-		whereclause += " and state=%s"
-		name_dict.append(request.GET['state'])
-		# name_dict['state'] = request.GET['state']
-	if 'year' in request.GET:
-		whereclause += " and year=%s"
-		name_dict.append(request.GET['year'])
-		# name_dict['year'] = request.GET['year']
+		if 'gender' in request.GET and request.GET['gender']!="":
+			whereclause += " and gender=%s"
+			name_dict.append(request.GET['gender'])
+		if request.GET['state']!="":
+			whereclause += " and state=%s"
+			name_dict.append(request.GET['state'])
+		if 'year' in request.GET and request.GET['gender']!="":
+			whereclause += " and year=%s"
+			name_dict.append(request.GET['year'])
 
-	query+=whereclause+groupby
-	print query, name_dict
-	names = Frequency.objects.raw(query, name_dict)[:100]
-	for n in names:
-		print n.name, n.count
-	return redirect(reverse('pop:index'))
+		query+=whereclause+groupby
+		print query, name_dict
+		names = Frequency.objects.raw(query, name_dict)[:100]
+		for n in names:
+			print n.name, n.count
+		return redirect(reverse('pop:index'))
