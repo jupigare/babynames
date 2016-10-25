e00e1029366d13669ce3ee37034acc844f61bfb2
Jasmine Singh <jasmine.singh@jas.si>
2016-10-24 18:00:20 -0700

    initial commit

---
+++ baby_names/baby_names/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'^', include('apps.login.urls', namespace="login")),
+    url(r'^faves', include('apps.faves.urls', namespace="faves")),
+    url(r'^random', include('apps.rand.urls', namespace="rand")),
+    url(r'^popular', include('apps.pop.urls', namespace="pop")),
+    url(r'^find', include('apps.find.urls', namespace="find")),
+]
+
+
+    
