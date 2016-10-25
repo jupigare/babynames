20260d92d239d1befca9f6b8a27a22fb499e788e
Jasmine Singh <jasmine.singh@jas.si>
2016-10-25 15:44:38 -0700

    popular names filter now functional

--- baby_names/apps/pop/views.py
+++ baby_names/apps/pop/views.py
@@ -4,21 +4,11 @@ from ..faves.models import Frequency
 
 class Index(View):
 	def get(self, request):
-		names = Frequency.objects.filter(state="CA",gender="M")[:10]
-		# newNames = []
-		# for i in range(0,10):
-		# 	newNames.append(names[i])
-
+		if 'query' not in request.session:
+			request.session['query'] = "SELECT id as id, name, sum(count) as count FROM frequency GROUP BY name ORDER BY sum(count) desc"
+			request.session['name_dict'] = []
+		names = Frequency.objects.raw(request.session['query'], request.session['name_dict'])[:100]
 		states = Frequency.objects.raw("select distinct state as id, state from frequency order by state")
-		# print allStates
-		# for s in allStates:
-		# 	print s.state
-
-		# states = [
-		# 	{'name': 'AK'},
-		# 	{'name': 'CA'}
-		# 	]
-
 		context = {
 			'names': names,
 			'states': states
@@ -28,24 +18,27 @@ class Index(View):
 class Filter(View):
 	def get(self, request):
 		print "Running filter..."
-		query = "SELECT name as id, name, sum(count) as count FROM frequency"
+		query = "SELECT id as id, name, sum(count) as count FROM frequency"
 		whereclause = " WHERE id>0"
 		groupby = " GROUP BY name ORDER BY sum(count) desc"
 		name_dict = []
 
-		if 'gender' in request.GET and request.GET['gender']!="":
-			whereclause += " and gender=%s"
-			name_dict.append(request.GET['gender'])
-		if request.GET['state']!="":
-			whereclause += " and state=%s"
-			name_dict.append(request.GET['state'])
-		if 'year' in request.GET and request.GET['gender']!="":
-			whereclause += " and year=%s"
-			name_dict.append(request.GET['year'])
+		if request.GET['submit']=="Filter":
+			if 'gender' in request.GET and request.GET['gender']!="":
+				whereclause += " and gender=%s"
+				name_dict.append(request.GET['gender'])
+			if request.GET['state']!="":
+				whereclause += " and state=%s"
+				name_dict.append(request.GET['state'])
+			if 'year' in request.GET and request.GET['year']!="":
+				whereclause += " and year=%s"
+				name_dict.append(request.GET['year'])
 
 		query+=whereclause+groupby
-		print query, name_dict
-		names = Frequency.objects.raw(query, name_dict)[:100]
-		for n in names:
-			print n.name, n.count
-		return redirect(reverse('pop:index'))
+		request.session['query'] = query
+		request.session['name_dict'] = name_dict
+		# names = Frequency.objects.raw(request.session['query'], request.session['name_dict'])[:100]
+		# names = Frequency.objects.raw(query, name_dict)[:100]
+		# for n in names:
+		# 	print n.name, n.count
+		return redirect('/popular')
