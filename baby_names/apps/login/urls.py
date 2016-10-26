818237e695727f2cc28d60ef13cdc1932729d36f
George Hsin <georgehsin@ucla.edu>
2016-10-25 17:21:10 -0700

    working routes and templates for login and faves

--- baby_names/apps/login/urls.py
+++ baby_names/apps/login/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.home, name = 'home'),
+    url(r'^register$', views.register, name = 'register'),
+    url(r'^login$', views.login, name = 'login'),
+    url(r'^loginRegistration$', views.index, name = 'loginRegistration'),
+    url(r'^logout$', views.logout, name = 'logout'),
+    # url(r'^success$', views.success), ===>>> go back to the home page
 ]
--- baby_names/apps/login/views.py
+++ baby_names/apps/login/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
-	pass
+	return render(request, 'login/index.html')
+
+def register(request):
+	result = User.objects.register(request.POST['user'], request.POST['passw'], request.POST['confirm'])
+	if result[0]:
+		request.session['id'] = result[1].id
+		print request.session['id']
+		return redirect('/')
+	else:
+		for error in result[1]:
+			messages.error(request, error)
+	return redirect('/home')
+
+def login(request):
+	result = User.objects.login(request.POST['user'], request.POST['password'])
+	if result[0]:
+		request.session['id'] = result[1].id
+		return redirect('/')
+	else:
+		for error in result[1]:
+			messages.error(request, error)
+	return redirect('/')
+
+def home(request):
+	return render(request, 'login/home.html')
+
+def logout(request):
+	request.session.flush()
+	return redirect('/')
