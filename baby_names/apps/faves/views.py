9665d1528262fcf5bbad5d53319b837087369177
George Hsin <georgehsin@ucla.edu>
2016-10-25 19:44:27 -0700

    display, add, and delete name on faves list

--- baby_names/apps/faves/views.py
+++ baby_names/apps/faves/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Frequency, Favorites
 from ..login.models import User
 
 # Create your views here.
 def index(request):
-	if 'freqid' in request.session:
-		favorite = Favorites.objects.filter(user_id = id)
+	# if 'freqid' in request.session:
+	# 	favorite = Favorites.objects.filter(user_id = request.session['id'])
+	names = Frequency.objects.all()
 	user = User.objects.get(id = request.session['id'])	
-	userlist = Frequency.objects.filter()
-	context = {'user':user}
-	return render(request, 'faves/index.html')
+	userlist = Favorites.objects.filter(user_id = request.session['id'])
+	print userlist
+	context = {'user':user, 'userlist': userlist, 'names':names}
+	return render(request, 'faves/index.html', context)
 
 def addfav(request, id):
-	request.session['freqid'] = id
-	user = User.objects.get(user_id = request.session['id'])	
-	userlist = Frequency.objects.get(frequency_id = id)
+	# request.session['freqid'] = id
+	name_id = id
+	print name_id
+	user = User.objects.get(id = request.session['id'])	
+	userlist = Frequency.objects.get(id = name_id)
 	Favorites.objects.create(user_id = user, frequency_id = userlist)
-	return redirect('/')
+	return redirect(reverse('faves:index'))
+
+def delfav(request, id):
+	name_id = id
+	user = User.objects.get(id = request.session['id'])	
+	userlist = Frequency.objects.get(id = name_id)
+	Favorites.objects.filter(user_id = user).filter(frequency_id = userlist).delete()
+	return redirect(reverse('faves:index'))
