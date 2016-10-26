ef4a2de1f173fd0d602324b99355b5dabc75ce48
Amy Wang <amywang200@gmail.com>
2016-10-25 17:51:57 -0700

    add random name generator

--- baby_names/apps/rand/views.py
+++ baby_names/apps/rand/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .. faves .models import Frequency
 # Create your views here.
+#
+import random
+
 def index(request):
-	pass
+    return render(request, 'rand/rand.html')
+
+# def length(database):
+#     while (database.name !=null):
+#         try:
+#             count ++
+#         except:
+#             count = 1
+#     return count
+
+def randName(request):
+    names = Frequency.objects.filter(gender = request.POST['gender'])
+    num = random.randint (0, len(names))
+    Rname = names[num].name
+    context = {
+    'name' : Rname
+    }
+    return render(request, 'rand/rand.html', context)
+     # return redirect(reverse('rand:randName'), context)
