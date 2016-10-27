d4d7ed7fadddf7ac777b7788e89cca7366e99698
Amy Wang <amywang200@gmail.com>
2016-10-27 10:32:02 -0700

    change random name generator

--- baby_names/apps/rand/views.py
+++ baby_names/apps/rand/views.py
@@ -16,26 +16,57 @@ def index(request):
 #             count = 1
 #     return count
 
-def randName(request):
-    if (request.POST['gender'] == '0'):
-        names = Frequency.objects.all()
+# def randName(request):
+#     if (request.POST['gender'] == '0'):
+#         names = Frequency.objects.all()
 
-    else:
-        names = Frequency.objects.filter(gender = request.POST['gender'])
-    x = len(names)
-    # print request.POST['gender']
-    print "length of name: ",len(names)
-    # default name is empty string
+#     else:
+#         names = Frequency.objects.filter(gender = request.POST['gender'])
+#     x = len(names)
+#     # print request.POST['gender']
+#     print "length of name: ",len(names)
+#     # default name is empty string
 
 
-    # have names
-    if len(names) > 0:
-        num = random.randint (0, len(names))
-        print num
-        Rname = names[num].name
+#     # have names
+#     if len(names) > 0:
+#         num = random.randint (0, len(names))
+#         print num
+#         Rname = names[num].name
 
-    context = {
-        'name' : Rname
-    }
-    return render(request, 'rand/rand.html', context)
+#     context = {
+#         'name' : Rname
+#     }
+#     return render(request, 'rand/rand.html', context)
      # return redirect(reverse('rand:randName'), context)
+def randName(request):
+    # print Frequency._meta.db_table
+    # Frequency.objects.raw("select distinct id, name, gender from frequency group by name")
+    # query = "SELECT COUNT(*) as id FROM frequency"
+    # results = Frequency.objects.raw(query)
+    # print results[0].id
+    # for i in results:
+    #     print i
+    #-------------the following are good!!!!$$$$$
+    query = "select distinct id, name, gender from frequency group by name"
+    results = Frequency.objects.raw(query)
+    listA = []
+    gender = request.POST['gender']
+    if (gender =='0'):
+        for i in results:
+            listA.append(i.name)
+    else:
+        for i in results:
+            if i.gender ==gender:
+                listA.append(i.name)
+
+    num = len(listA)
+    x = random.randint(0, num-1)
+    Rname = listA[x]
+    users = Frequency.objects.filter(name = Rname)
+    context = {
+    "name": Rname,
+    "id": users[0].id
+    }
+
+    return render(request, 'rand/rand.html', context)
