7f829521e465a30ed7fbfd018478b46473f6ad14
Jasmine Singh <jasmine.singh@jas.si>
2016-10-27 18:13:25 -0700

    cleaned up code

--- baby_names/apps/find/views.py
+++ baby_names/apps/find/views.py
@@ -17,7 +17,7 @@ class Find(View):
 		request.session['nameSearch'] = name[0].upper() + name[1:].lower()
 		return redirect(reverse('find:results', kwargs={'name': name}))
 	def post(self, request, name):
-		if 'nameSearch' in request.POST and request.POST['nameSearch'] != "":
+		if 'nameSearch' in request.POST and len(request.POST['nameSearch'])>0:
 			name = request.POST['nameSearch']
 			request.session['nameSearch'] = name[0].upper() + name[1:].lower()
 			return redirect(reverse('find:results', kwargs={'name': name}))
@@ -57,19 +57,6 @@ class Results(View):
 				count=count,
 			)
 		)
-		# hover = HoverTool(tooltips="""
-	 #		<table class="tooltips">
-	 #			<tr>
-	 #				<td>Birth Year</td>
-	 #				<td>@year</td>
-	 #			</tr>
-	 #			<tr>
-	 #				<td>Count</td>
-	 #				<td>@count</td>
-	 #			</tr>
-	 #		</table>
-	 #		"""
-		# )
 		p = figure(plot_width=800, plot_height=450, tools=['hover','pan','wheel_zoom','reset','save'], active_drag="pan", active_scroll="wheel_zoom")
 		p.line(year,count,source=source, line_width=2)
 		hover = p.select(dict(type=HoverTool))
@@ -112,44 +99,3 @@ class Reset(View):
 			del request.session['nameSearch']
 			request.session['nameSearch'].clear()
 		return redirect(reverse('find:index'))
-
-# Create your views here.
-# def index(request):
-#	return render(request, 'find/index.html')
-
-# def find(request):
-# 	if request.method == "POST":
-# 		print("find")
-# 		nameSearch = request.POST['nameSearch']
-# 		results = Frequency.objects.filter(name=nameSearch)
-# 		print(results)
-# 		for r in results:
-# 			print r.name, r.id
-# 		return nameResults(request, nameSearch, results)
-# 		# return render(request, 'find/find.html', context)
-		
-# def nameResults(request, nameSearch, names):
-# 	print("nameResults")
-# 	print(names)
-# 	request.session['nameSearch']=nameSearch
-# 	context = {
-# 		'year': 2000,
-# 		'count': 1
-# 	}
-# 	return render(request, 'find/index.html', context)
-
-# def addfav(request, id):
-# 	# request.session['freqid'] = id
-# 	name_id = id
-# 	print name_id
-# 	user = User.objects.get(id = request.session['id'])	
-# 	userlist = Frequency.objects.get(id = name_id)
-# 	Favorites.objects.create(user_id = user, frequency_id = userlist)
-# 	return redirect(reverse('faves:index'))
-
-# def delfav(request, id):
-# 	name_id = id
-# 	user = User.objects.get(id = request.session['id'])	
-# 	userlist = Frequency.objects.get(id = name_id)
-# 	Favorites.objects.filter(user_id = user).filter(frequency_id = userlist).delete()
-# 	return redirect(reverse('faves:index'))
