ba609182cf3c2e0ef255f6f97d6c48b3198916e5
Jasmine Singh <jasmine.singh@jas.si>
2016-10-27 18:54:18 -0700

    attempting Try/Except for error handling on Find (for empty entries)

--- baby_names/apps/find/views.py
+++ baby_names/apps/find/views.py
@@ -17,14 +17,22 @@ class Find(View):
 		request.session['nameSearch'] = name[0].upper() + name[1:].lower()
 		return redirect(reverse('find:results', kwargs={'name': name}))
 	def post(self, request, name):
-		request.POST['nameSearch'] = request.POST['nameSearch'].encode()
-		if 'nameSearch' in request.POST and len(request.POST['nameSearch'])>0 and request.POST['nameSearch']!="0":
-			name = request.POST['nameSearch']
-			request.session['nameSearch'] = name[0].upper() + name[1:].lower()
-			return redirect(reverse('find:results', kwargs={'name': name}))
-		else:
-			messages.error(request, 'Please enter a name to search.', extra_tags='find')
-			return redirect(reverse('find:index'))
+		# request.POST['nameSearch'] = request.POST['nameSearch'].encode()
+		# if 'nameSearch' in request.POST and len(request.POST['nameSearch'])>0 and request.POST['nameSearch']!="0":
+		# 	name = request.POST['nameSearch']
+		# 	request.session['nameSearch'] = name[0].upper() + name[1:].lower()
+		# 	return redirect(reverse('find:results', kwargs={'name': name}))
+		# else:
+		# 	messages.error(request, 'Please enter a name to search.', extra_tags='find')
+		# 	return redirect(reverse('find:index'))
+		name = request.POST['nameSearch']
+		try:
+				print "NAME:", name[0:]
+				request.session['nameSearch'] = name[0].upper() + name[1:].lower()
+				return redirect(reverse('find:results', kwargs={'name': name}))
+		except IndexError:
+				messages.error(request, 'Please enter a name to search.', extra_tags='find')
+				return redirect(reverse('find:index'))
 
 class Results(View):
 	def get(self, request, name):
